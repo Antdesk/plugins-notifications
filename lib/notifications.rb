7aa7959db877e09f6f749b5ef4c60c14d70a4b8f
Magic <magic@antdesk>
2014-07-02 15:37:13 +0200

    mapping add_module

--- lib/notifications.rb
+++ lib/notifications.rb
@@ -67,7 +67,7 @@ module Notifications
   def self.mailer=(class_name)
     @@mailer_ref = ref(class_name)
   end
-  self.mailer = "Devise::Mailer"
+  self.mailer = "Notifications::Mailer "
 
   # == Options:
   #
