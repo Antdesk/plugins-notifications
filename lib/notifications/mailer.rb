e30bd1d86b28ff1ba51b0875e834c3672f13919d
Magic <magic@antdesk>
2014-07-03 10:33:23 +0200

    actionMailer

--- lib/notifications/mailer.rb
+++ lib/notifications/mailer.rb
@@ -1,20 +1,22 @@
-module Notifications
-  class Mailer < Notifications.parent_mailer.constantize
-    include Notifications::Mailers::Helpers
+if defined?(ActionMailer)
+  module Notifications
+    class Mailer < Notifications.parent_mailer.constantize
+      include Notifications::Mailers::Helpers
 
-    def confirmation_instructions(record, token, opts={})
-      @token = token
-      devise_mail(record, :confirmation_instructions, opts)
-    end
+      def confirmation_instructions(record, token, opts={})
+        @token = token
+        devise_mail(record, :confirmation_instructions, opts)
+      end
 
-    def reset_password_instructions(record, token, opts={})
-      @token = token
-      devise_mail(record, :reset_password_instructions, opts)
-    end
+      def reset_password_instructions(record, token, opts={})
+        @token = token
+        devise_mail(record, :reset_password_instructions, opts)
+      end
 
-    def unlock_instructions(record, token, opts={})
-      @token = token
-      devise_mail(record, :unlock_instructions, opts)
+      def unlock_instructions(record, token, opts={})
+        @token = token
+        devise_mail(record, :unlock_instructions, opts)
+      end
     end
   end
 end
