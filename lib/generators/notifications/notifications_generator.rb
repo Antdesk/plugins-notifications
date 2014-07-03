1f14602a73e39f713278d56032446ffc0361b319
Magic <magic@antdesk>
2014-07-03 15:40:04 +0200

    NotificationsGenerator

--- lib/generators/notifications/notifications_generator.rb
+++ lib/generators/notifications/notifications_generator.rb
@@ -2,11 +2,11 @@ require 'rails/generators/named_base'
 
 module Notifications
   module Generators
-    class DeviseGenerator < Rails::Generators::NamedBase
+    class NotificationsGenerator < Rails::Generators::NamedBase
       include Rails::Generators::ResourceHelpers
 
       namespace "notifications"
-      #source_root File.expand_path("../templates", __FILE__)
+      source_root File.expand_path("../templates", __FILE__)
 
       desc "Edit a model with the given NAME (if one does exist) with notifications " <<
            "configuration."
